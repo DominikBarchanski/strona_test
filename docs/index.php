77963f4d5cea152c88e3741734a291378e9089f4
DominikBarchanski <39733874+DominikBarchanski@users.noreply.github.com>
2021-01-13 19:13:51 +0100

    Update and rename index.md to index.php
    test

---
+++ docs/index.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+  <link rel="stylesheet" href="stylecs.css" type="text/css"/>
+  <meta http-equiv="content-type" content="text/html" charset="utf-8"/>
+
+  <title >ele </title>
+
+</head>
+<body bgcolor="gray">
+  <div id="full_site" align="center">
+  <div id="top_menu">
+    <ul>
+      <li><a href="#">strona główna</a></li>
+      <li><a href="#">menu</a></li>
+      <li><a href="#">dodaj fakt</a></li>
+      <li><a href="#">wyloguj się</a></li>
+    </ul>
+    <p>góra</p>
+  </div>
+  <div id="content">
+<p>srodek</p>
+  </div>
+  <div id="foot">
+<p>stopka</p>
+  </div>
+
+</div>
+</body>
+</html>
